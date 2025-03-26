46477d36c481b97b9cd9cb8dbc35ab8d34ddbb94
Abdul Moeed <moeeda205@gmail.com>
2025-03-26 12:50:56 +0500

    Updated settings.py for production

--- docride/settings.py
+++ docride/settings.py
@@ -1,12 +1,13 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-4=#!(8jw97a-^%dpnqi)0x%#)hw_j$_l9fj$764ozd-10evhcp'
+SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-dev-key")
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend-docride.onrender.com', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -32,8 +33,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
+    'http://localhost:5173',               # (keep for local development)
+    'https://docride.vercel.app',         # (your production frontend)
 ]
+
 
 ROOT_URLCONF = 'docride.urls'
 
@@ -87,9 +90,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-SITE_URL = 'http://localhost:5173'
+SITE_URL = 'https://docride.vercel.app'
 
 STRIPE_SECRET_KEY = 'sk_test_51R3Q1PEYQxHtXYU4rAo2IAxXrezUQ8Cmwk7hsH5A2qOlekKC2NM26b82pnyjeSMRWcIeFd5PBLcLgsAs7upo9Ysr00MNYn2ID0'
 
